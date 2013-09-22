1b8caf8654cddab63c6ba9a2fc27f50f76317397
Jeff Wolski <wolski@uber.com>
2013-09-21 20:11:15 -0700

    More commands

--- graphite-cli.js
+++ graphite-cli.js
@@ -10,15 +10,23 @@ var graphiteUrl;
 commander.version('0.0.1')
     .option('-c, --conf <conf>', 'Configuration file', null, './graphite-config.js');
 
-commander.command('cp [source] [target]')
+commander.command('cat <dashboard>')
+    .description('Dumps raw dashboard definition')
+    .action(cat);
+
+commander.command('cp <source> <target>')
     .description('Copy source dashboard to target dashboard')
     .action(cp);
+
+commander.command('load <dashboard>')
+    .description('Alias for `cat`')
+    .action(cat);
 
 commander.command('ls [search]')
     .description('List dashboards')
     .action(ls);
 
-commander.command('mv [source] [target]')
+commander.command('mv <source> <target>')
     .description('Move source dashboard to target dashboard')
     .action(mv);
 
@@ -39,8 +47,37 @@ function init() {
     graphiteUrl = util.format('http://%s:%d', hostname, port);
 }
 
-function cp(source, target) {
+function cat(dashboard, callback) {
+    init();
 
+    var options = {
+        url: graphiteUrl + '/dashboard/load/' + dashboard
+    };
+
+    request.get(options, function(err, resp, body) {
+        if (err) {
+            console.log('ERROR: ' + err.message);
+            return;
+        } else {
+            if (resp.statusCode === 200) {
+                console.log(body);
+            } else {
+                console.log('Not deleted ;(');
+            }
+        }
+
+        if (callback && typeof callback === 'function') {
+            callback(err, resp, body);
+        }
+    });
+}
+
+function cp(source, target, callback) {
+    cat(source, function(err, resp, body) {
+        if (resp.statusCode === 200) {
+            save(target, JSON.parse(body).state, callback);
+        }
+    });
 }
 
 function ls(search) {
@@ -70,7 +107,9 @@ function ls(search) {
 }
 
 function mv(source, target) {
-
+    cp(source, target, function() {
+        rm(source);
+    });
 }
 
 function rm(dashboard) {
@@ -93,41 +132,11 @@ function rm(dashboard) {
     });
 }
 
-function touch(dashboard) {
+function save(dashboard, state, callback) {
     init();
 
     var options = {
-        form: {
-            state: JSON.stringify({
-                name: dashboard,
-                defaultGraphParams: {
-                    from: '-2weeks',
-                    until: '-',
-                    height: '300',
-                    width: '400'
-                },
-                graphSize: {
-                    height: 250,
-                    width: 400
-                },
-                graphs: [],
-                refreshConfig: {
-                    enabled: false,
-                    interval: 60000
-                },
-                timeConfig: {
-                    type: 'relative',
-                    relativeStartQuantity: '2',
-                    relativeStartUnits: 'weeks',
-                    relativeUntilQuantity: '',
-                    relativeUntilUnits: 'now',
-                    startDate: '2013-09-21T19:03:12',
-                    startTime: '9:00AM',
-                    endDate: '2013-09-21T19:03:12',
-                    endTime: '5:00PM'
-                }
-            })
-        },
+        form: { state: JSON.stringify(state) },
         url: graphiteUrl + '/dashboard/save/' + dashboard
     };
 
@@ -137,9 +146,44 @@ function touch(dashboard) {
             return;
         } else {
             if (resp.statusCode === 200) {
+                if (callback && typeof callback === 'function') {
+                    callback();
+                }
             } else {
                 console.log('Not created: ' + body);
             }
         }
+    });
+}
+
+function touch(dashboard) {
+    save(dashboard, {
+            name: dashboard,
+            defaultGraphParams: {
+                from: '-2weeks',
+                until: '-',
+                height: '300',
+                width: '400'
+            },
+            graphSize: {
+                height: 250,
+                width: 400
+            },
+            graphs: [],
+            refreshConfig: {
+                enabled: false,
+                interval: 60000
+            },
+            timeConfig: {
+                type: 'relative',
+                relativeStartQuantity: '2',
+                relativeStartUnits: 'weeks',
+                relativeUntilQuantity: '',
+                relativeUntilUnits: 'now',
+                startDate: '2013-09-21T19:03:12',
+                startTime: '9:00AM',
+                endDate: '2013-09-21T19:03:12',
+                endTime: '5:00PM'
+            }
     });
 }
